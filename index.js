facf01982a2851a345b65b57c1326df866820254
Asjid Kalam <asjid.kalam@gmail.com>
2021-01-10 15:40:27 +0530

    fixed prototype pollution

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const realTypeOf = require('realtypeof');
 const deeplyObjectAssign = (...objects) =>
   objects.reduce((acc, obj) => {
     Object.keys(obj).forEach(key => {
+      if(key.includes('__proto__') || key.includes('constructor') || key.includes('prototype')){
+        return acc;
+      }
       if (realTypeOf.isObject(acc[key]) && realTypeOf.isObject(obj[key]))
         return deeplyObjectAssign(acc[key], obj[key]);
       acc[key] = obj[key];
